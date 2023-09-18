f3bea6eab54ad53a2fcd1f0408b38a382985754f
Shane Mindreau <smindre1@binghamton.edu>
2023-09-18 18:25:42 -0400

    I added an event listener to the submit button and edited the html user input value to what I believe would fit in the Geo API Url. I now need to introduce other variables to fill portions of the API url's endpoint.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,48 @@
 //Variables
 
-const APIKey = '4c58e25ed978176bdd5f665c64121ffb';
+const APIKey = "4c58e25ed978176bdd5f665c64121ffb";
 
 //404 check
 
-
 //input event listener
-
+$("#submitBtn").on("click", function () {
+  let input = $("#searchBar").val();
+  for (i = 0; i < input.length; i++) {
+    if (input[i] === " ") {
+      input = input.replaceAll(" ", "+");
+    }
+  }
+  locationCoords(input);
+});
+//Fix the enter key event listener
+$("#searchBar").keypress(function (e) {
+  if (e == 13) {
+    let input = $("#searchBar").val();
+    for (i = 0; i < input.length; i++) {
+      if (input[i] === " ") {
+        input = input.replaceAll(" ", "+");
+      }
+      locationCoords(input);
+    }
+  }
+});
 //geo fetch
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city name},${state code},${country code}&limit=${limit}&appid=${API key}`)
+function locationCoords() {
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=${APIKey}`)
     .then((res) => res.json)
     .then((res) => {
-        console.log(res);
-    })
-//weather fetch
-let lat = "";
-let lon = "";
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`)
+      console.log(res);
+    });
+
+  //weather fetch
+  let lat = "";
+  let lon = "";
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`)
     .then((res) => res.json)
     .then((res) => {
-        console.log(res);
-    })
+      console.log(res);
+    });
+}
 //then function
 
 //local storage function
